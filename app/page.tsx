f35495bb6b09a2f6c10f5cbe995cccab810611ff
JoaoDePaulo <joaodepaulocardoso@edu.unifor.br>
2025-10-01 12:33:01 -0300

    feat: add description and logo-2

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,25 @@ export default function Home() {
         priority
       />
       <Menu />
+      <div className="relative w-full h-[800px] items-center flex">
+        <Image
+          src="/spider-man.png"
+          alt="spider-man"
+          fill
+          className="object-top object-contain"
+        />
+        <div className="z-10 items-center justify-center flex flex-col mt-20">
+          <Image src="/logo-2.png" alt="logo" width={180} height={88} />
+          <p>
+            Miles Morales é o mais novo Homem-Aranha de Nova York da Marvel.
+            Após a morte prematura de seu pai, Miles foi apresentado a Peter
+            Parker, que rapidamente se tornou seu amigo e mentor. Quando Miles
+            foi picado por uma aranha geneticamente modificada da Oscorp, ele
+            desenvolveu poderes únicos e, após meses de insistência, Peter
+            concordou em treiná-lo.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
